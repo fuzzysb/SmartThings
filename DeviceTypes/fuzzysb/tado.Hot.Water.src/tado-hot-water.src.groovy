9f03f0c7a4f50de2eea0c2f8ffddf844c6c8a50c
fuzzysb <stuartb@broadbandtap.co.uk>
2016-04-25 21:23:20 +0100

    v1.3

--- DeviceTypes/fuzzysb/tado.Hot.Water.src/tado-hot-water.src.groovy
+++ DeviceTypes/fuzzysb/tado.Hot.Water.src/tado-hot-water.src.groovy
@@ -13,6 +13,7 @@
  *	Tado Thermostat
  *
  * 	Author: Stuart Buchanan, Based on original work by Ian M with thanks. also source for icons was from @tonesto7's excellent Nest Manager.
+ * 	Date: 2016-04-25 v1.3 Finally found time to update this with the lessons learnt from the Tado Cooling Device Type. will bring better support for RM and Thermostat Director
  * 	Date: 2016-04-08 v1.2 added setThermostatMode(mode) function to work better with Rule Machine and Thermostat Mode Director
  *	Date: 2016-04-05 v1.1 change of default Water Heating Temps can now be defined in device preferences (default Value is 90C). 
  *	Date: 2016-04-05 v1.0 Initial release
@@ -37,7 +38,9 @@ metadata {
 		capability "Polling"
 		capability "Refresh"
         
-        
+        attribute "tadoMode", "string"
+		command "temperatureUp"
+        command "temperatureDown"
         command "heatingSetpointUp"
         command "heatingSetpointDown"
 		command "on"
@@ -58,20 +61,29 @@ tiles(scale: 2){
 			tileAttribute("device.temperature", key:"PRIMARY_CONTROL", canChangeIcon: true, canChangeBackground: true){
             	attributeState "default", label:'${currentValue}°', backgroundColor:"#fab907", icon:"https://raw.githubusercontent.com/fuzzysb/SmartThings/master/DeviceTypes/fuzzysb/tado.Hot.Water.src/Images/tap_icon.png"
             }
+			tileAttribute("device.temperature", key: "VALUE_CONTROL") {
+    			attributeState("VALUE_UP", action: "temperatureUp")
+    			attributeState("VALUE_DOWN", action: "temperatureDown")
+  			}
             tileAttribute("device.thermostatOperatingState", key: "OPERATING_STATE") {
-    			attributeState("SLEEP", label:'${name}', backgroundColor:"#0164a8")
-    			attributeState("HOME", label:'${name}', backgroundColor:"#fab907")
-    			attributeState("AWAY", label:'${name}', backgroundColor:"#62aa12")
-                attributeState("OFF", label:'${name}', backgroundColor:"#c0c0c0")
-                attributeState("MANUAL", label:'${name}', backgroundColor:"#804000")
-			}
+    			attributeState("idle", backgroundColor:"#666666")
+    			attributeState("heating", backgroundColor:"#ff471a")
+                attributeState("emergency heat", backgroundColor:"#ff471a")
+  			}
+			tileAttribute("device.thermostatMode", key: "THERMOSTAT_MODE") {
+    			attributeState("off", label:'${name}')
+    			attributeState("heat", label:'${name}')
+  			}
+			tileAttribute("device.heatingSetpoint", key: "HEATING_SETPOINT") {
+    			attributeState("default", label:'${currentValue}', unit:"dF")
+  			}
 		}
-        
+		
         valueTile("heatingSetpoint", "device.heatingSetpoint", width: 2, height: 1, decoration: "flat") {
 			state "default", label: 'Set Point\r\n\${currentValue}°'
 		}
 
-        standardTile("thermostatOperatingState", "device.thermostatOperatingState", width: 2, height: 2, canChangeIcon: true, canChangeBackground: true) {         
+        standardTile("tadoMode", "device.tadoMode", width: 2, height: 2, canChangeIcon: true, canChangeBackground: true) {         
 			state("SLEEP", label:'${name}', backgroundColor:"#0164a8", icon:"st.Bedroom.bedroom2")
             state("HOME", label:'${name}', backgroundColor:"#fab907", icon:"st.Home.home2")
             state("AWAY", label:'${name}', backgroundColor:"#62aa12", icon:"st.Outdoor.outdoor18")
@@ -80,8 +92,8 @@ tiles(scale: 2){
 		}
     	
 		standardTile("thermostatMode", "device.thermostatMode", width: 2, height: 2, canChangeIcon: true, canChangeBackground: true) {
-        	state("HEAT", label:'${name}', backgroundColor:"#ea2a2a", icon:"https://raw.githubusercontent.com/fuzzysb/SmartThings/master/DeviceTypes/fuzzysb/tado.Hot.Water.src/Images/heat_mode_icon.png")
-            state("OFF", label:'', backgroundColor:"#ffffff", icon:"https://raw.githubusercontent.com/fuzzysb/SmartThings/master/DeviceTypes/fuzzysb/tado.Hot.Water.src/Images/hvac_off.png", defaultState: true)  
+        	state("heat", label:'HEAT', backgroundColor:"#ea2a2a", icon:"https://raw.githubusercontent.com/fuzzysb/SmartThings/master/DeviceTypes/fuzzysb/tado.Hot.Water.src/Images/heat_mode_icon.png")
+            state("off", label:'', backgroundColor:"#ffffff", icon:"https://raw.githubusercontent.com/fuzzysb/SmartThings/master/DeviceTypes/fuzzysb/tado.Hot.Water.src/Images/hvac_off.png", defaultState: true)  
 		}
 		
         standardTile("refresh", "device.switch", width: 2, height: 1, decoration: "flat") {
@@ -109,7 +121,7 @@ tiles(scale: 2){
             state("default", label:'', action:"endManualControl", icon:"https://raw.githubusercontent.com/fuzzysb/SmartThings/master/DeviceTypes/fuzzysb/tado.Hot.Water.src/Images/endManual.png")
 		}
 		main "thermostat"
-		details (["thermostat","thermostatMode","outsidetemperature","heatingSetpoint","refresh","heatingSetpointUp","heatingSetpointDown","thermostatOperatingState","emergencyHeat","heat","Off","endManualControl"])
+		details (["thermostat","thermostatMode","outsidetemperature","heatingSetpoint","refresh","heatingSetpointUp","heatingSetpointDown","tadoMode","emergencyHeat","heat","Off","endManualControl"])
 	}
 }
 def updated(){
@@ -117,7 +129,6 @@ def updated(){
 	getidCommand(),
 	getTempUnitCommand(),
 	getCapabilitiesCommand(),
-	refresh()
 	]
 	delayBetween(cmds, 2000)
 }
@@ -233,6 +244,7 @@ private parseResponse(resp) {
     def ACMode
     def ACFanSpeed
     def thermostatSetpoint
+	def tOperatingState
     if(resp.status == 200) {
         log.debug("Executing parseResponse.successTrue")
         def temperature
@@ -259,17 +271,18 @@ private parseResponse(resp) {
         }else if(resp.data.overlayType == "MANUAL"){
         	autoOperation = "MANUAL"
         }
-        log.debug("Read thermostatOperatingState: " + autoOperation)
-        sendEvent(name: 'thermostatOperatingState', value: autoOperation)
+        log.debug("Read tadoMode: " + autoOperation)
+        sendEvent(name: 'tadoMode', value: autoOperation)
        
-		if (resp.data.tadoMode == "HOME" && resp.data.setting.power == "ON"){
-			sendEvent(name: 'thermostatMode', value: "HEAT")
+		if (resp.data.setting.power == "ON"){
+			sendEvent(name: 'thermostatMode', value: "heat")
+			sendEvent(name: 'thermostatOperatingState', value: "heating")
 			log.debug("Send thermostatMode Event Fired")
 		} else if(resp.data.setting.power == "OFF"){
-			sendEvent(name: 'thermostatMode', value: "OFF")
+			sendEvent(name: 'thermostatMode', value: "off")
+			sendEvent(name: 'thermostatOperatingState', value: "idle")
 			log.debug("Send thermostatMode Event Fired")
 		}
-		sendEvent(name: 'thermostatOperatingState', value: autoOperation)
         log.debug("Send thermostatMode Event Fired")
 		if (state.supportsWaterTempControl == "true"){
 			if (temperatureUnit == "C") {
@@ -517,9 +530,15 @@ def autoCommand(){
     def initialsetpointtemp
 	def terminationmode = settings.manualmode
 	def jsonbody
+	def traperror
 	if(state.supportsWaterTempControl == "true"){ 
-		if(device.currentValue("thermostatSetpoint") == 0 ){
-			initialsetpointtemp = device.currentValue("temperature")
+		try {
+			traperror = ((device.currentValue("thermostatSetpoint")).intValue())
+		}catch (NumberFormatException e){
+			traperror = 0 
+		}
+		if(traperror == 0){
+			initialsetpointtemp = settings.defHeatingTemp
 		} else {
 			initialsetpointtemp = device.currentValue("thermostatSetpoint")
 		}
@@ -568,9 +587,15 @@ def heatCommand(){
 	def terminationmode = settings.manualmode
 	def jsonbody
     def initialsetpointtemp
+	def traperror
 	if(state.supportsWaterTempControl == "true"){ 
-		if(device.currentValue("thermostatSetpoint") == 0){
-			initialsetpointtemp = settings.defWaterTemp
+		try {
+			traperror = ((device.currentValue("thermostatSetpoint")).intValue())
+		}catch (NumberFormatException e){
+			traperror = 0 
+		}
+		if(traperror == 0){
+			initialsetpointtemp = settings.defHeatingTemp
 		} else {
 			initialsetpointtemp = device.currentValue("thermostatSetpoint")
 		}
@@ -585,9 +610,15 @@ def emergencyHeat(){
 	log.debug "Executing 'sendCommand.heatCommand'"
     def initialsetpointtemp
 	def jsonbody
+	def traperror
 	if(state.supportsWaterTempControl == "true"){ 
-	    if(device.currentValue("thermostatSetpoint") == 0){
-			initialsetpointtemp = settings.defWaterTemp
+	    try {
+			traperror = ((device.currentValue("thermostatSetpoint")).intValue())
+		}catch (NumberFormatException e){
+			traperror = 0 
+		}
+		if(traperror == 0){
+			initialsetpointtemp = settings.defHeatingTemp
 		} else {
 			initialsetpointtemp = device.currentValue("thermostatSetpoint")
 		}
